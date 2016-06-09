48cc0d5de771f0e903758765326c4df121e6a1b3
Munish <Munish@MUNISH>
2016-06-09 16:29:34 +0530

    first commit

---
+++ patterns/src/patterns/mun3.java
@@ -0,0 +1,15 @@
+package patterns;
+
+public class mun3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5; j >= i; j--) {
+				System.out.print(j + "  ");
+			}
+		System.out.print("    ");
+		}
+
+	}
+
+}
